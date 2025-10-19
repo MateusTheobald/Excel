e17326e4d72d931dba6fbcbbbd311d04a6a6d019
Mateus Theobald <141860190+MateusTheobald@users.noreply.github.com>
2025-10-18 23:40:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = new WebSocket(`ws://localhost:3000`);
 const connectBtn = document.getElementById("connectBtn");
 
 const gauges = {
